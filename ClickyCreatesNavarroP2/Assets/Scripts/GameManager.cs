711db07cf85a7df49d71b3771ccf674311757b27
M4ND017 <732296@student.dusd.net>
2025-05-20 09:56:30 -0700

    5.2
    i finished 5.1 instructions and im working on 5.2 instructions (on number 4)

---
+++ ClickyCreatesNavarroP2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+
+    }
+}
+
+
